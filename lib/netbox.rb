1a0ccf3aa5d732470e46484e961e8777d6d44110
Motoyuki OHMORI <ohmori@tottori-u.ac.jp>
2025-07-20 05:54:28 +0900

    initial commit.

---
+++ lib/netbox.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "netbox/version"
+
+module Netbox
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/netbox/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Netbox
+  VERSION = "0.1.0"
+end
